0fb31201d9c2fcb21a90b8a3d5107ebb76fc26a5
edutim <timothy.hart@gmail.com>
2024-01-22 13:46:45 -0500

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Timothy Hart on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
